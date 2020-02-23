256491e16401f492c7960b219a88fd2e6a1cb644
Kenji Maeda <maeken@nexmedia.jp>
2020-02-23 21:02:30 +0900

    更新 - FIELD_LIST複数対応 - フィールド名の大文字対応

--- src/dtomaker.ts
+++ src/dtomaker.ts
@@ -15,13 +15,17 @@ const PATTERN_ENGINE = /\{\{engine\}\}/g;
 /** 正規表現パターン：プライマリID */
 const PATTERN_PRIMARY_ID = /\{\{primary_id\}\}/g;
 /** 正規表現パターン：フィールドリスト */
-const PATTERN_FIELD_LIST = /<<<fields_list(\r\n|\n)(.*)>>>fields_list(\r\n|\n)/s;
+const PATTERN_FIELD_LIST_WHOLE = /<<<fields_list(\r\n|\n).*?>>>fields_list(\r\n|\n)/gs;
+/** 正規表現パターン：フィールドリスト */
+const PATTERN_FIELD_LIST_PARTS = /<<<fields_list(\r\n|\n)(.*?)>>>fields_list(\r\n|\n)/s;
 /** 正規表現パターン(フィールド)：フィールドコメント */
 const PATTERN_FIELD_COMMENT = /\{\{field_comment\}\}/g;
 /** 正規表現パターン(フィールド)：フィールド型 */
 const PATTERN_FIELD_TYPE = /\{\{field_type\}\}/g;
 /** 正規表現パターン(フィールド)：フィールド名 */
 const PATTERN_FIELD_NAME = /\{\{field_name\}\}/g;
+/** 正規表現パターン(フィールド)：フィールド名(大文字) */
+const PATTERN_FIELD_NAME_UPPERCASE = /\{\{field_name_uppercase\}\}/g;
 /** 正規表現パターン(フィールド)：Null許容 */
 const PATTERN_FIELD_NULLABLE = /\{\{field_nullable\}\}/g;
 /** 正規表現パターン(フィールド)：フィールドの既定値 */
@@ -78,7 +82,7 @@ class Template
 {
     /** テンプレートコンテンツ */
     template: string = '';
-    fieldTemplate: string = '';
+    fieldTemplates: Array<string> = [];
     classNameFormat: string = '';
     ltrimTableName: string = '';
     dataTypeFinder: DataTypeFinder;
@@ -94,11 +98,12 @@ class Template
         this.dataTypeFinder = new DataTypeFinder(config.format.defaultValues);
 
         // テンプレートからフィールドテンプレートを抜き出す
-        const matches = this.template.match(PATTERN_FIELD_LIST);
-        if (!matches) {
-            throw new Error('fields_listキーワード書いてないんじゃない？');
-        }
-        this.fieldTemplate = matches[2];
+        let counter = 1;
+        this.template = this.template.replace(PATTERN_FIELD_LIST_WHOLE, (match) => {
+            const fieldListContents = match.match(PATTERN_FIELD_LIST_PARTS) || [];
+            this.fieldTemplates.push(fieldListContents[2] || '');
+            return `FIELD_LIST_NO_${counter++}`;
+        });
     }
 
     /**
@@ -107,7 +112,7 @@ class Template
      * @returns DTOWriter
      */
     public createMaker(dbTable: DBTable) {
-        return new DTOWriter(this.template, this.fieldTemplate, this.createClassName(dbTable.name), dbTable, this.dataTypeFinder);
+        return new DTOWriter(this.template, this.fieldTemplates, this.createClassName(dbTable.name), dbTable, this.dataTypeFinder);
     }
 
     /**
@@ -150,7 +155,7 @@ class DTOWriter {
     /** コンテンツ */
     private content: string = '';
     /** フィールドテンプレート */
-    private fieldTemplate: string = '';
+    private fieldTemplates: Array<string> = [];
     /** クラス名 */
     private className: string = '';
     /** テーブル名 */
@@ -158,22 +163,25 @@ class DTOWriter {
     /** プライマリフィールド名 */
     private primaryKey: string = '';
     /** 置き換えフィールド保持配列 */
-    private replaceFields: Array<string> = [];
+    private replaceFields: Array<Array<string>> = [];
     /** デフォルト値 */
     private dataTypeFinder: DataTypeFinder;
 
     /**
      * コンストラクタ
      * @param content コンテンツ
-     * @param fieldTemplate フィールドリストテンプレート
+     * @param fieldTemplates フィールドリストテンプレート
      * @param className テーブル名
      */
-    public constructor(content: string, fieldTemplate: string, className: string, tableInfo: DBTable, dataTypeFinder: DataTypeFinder) {
+    public constructor(content: string, fieldTemplates: Array<string>, className: string, tableInfo: DBTable, dataTypeFinder: DataTypeFinder) {
         this.content = content;
-        this.fieldTemplate = fieldTemplate;
+        this.fieldTemplates = fieldTemplates;
         this.className = className;
         this.tableInfo = tableInfo;
         this.dataTypeFinder = dataTypeFinder;
+        for (let i = 0; i < fieldTemplates.length; i++) {
+            this.replaceFields.push([]);
+        }
     }
 
     /**
@@ -185,13 +193,15 @@ class DTOWriter {
             this.primaryKey = fieldInfo.field;
         }
         const dataType = this.dataTypeFinder.find(fieldInfo);
-        let tmpField = this.fieldTemplate;
-        tmpField = tmpField.replace(PATTERN_FIELD_COMMENT, fieldInfo.comment);
-        tmpField = tmpField.replace(PATTERN_FIELD_TYPE, dataType.label);
-        tmpField = tmpField.replace(PATTERN_FIELD_NAME, fieldInfo.field);
-        tmpField = tmpField.replace(PATTERN_FIELD_NULLABLE, fieldInfo.null);
-        tmpField = tmpField.replace(PATTERN_FIELD_DEFAULT_VALUE, dataType.defaultValue);
-        this.replaceFields.push(tmpField);
+        this.fieldTemplates.forEach((tmpField, index) => {
+            tmpField = tmpField.replace(PATTERN_FIELD_COMMENT, fieldInfo.comment);
+            tmpField = tmpField.replace(PATTERN_FIELD_TYPE, dataType.label);
+            tmpField = tmpField.replace(PATTERN_FIELD_NAME, fieldInfo.field);
+            tmpField = tmpField.replace(PATTERN_FIELD_NAME_UPPERCASE, fieldInfo.field.toUpperCase());
+            tmpField = tmpField.replace(PATTERN_FIELD_NULLABLE, fieldInfo.null);
+            tmpField = tmpField.replace(PATTERN_FIELD_DEFAULT_VALUE, dataType.defaultValue);
+            this.replaceFields[index].push(tmpField);
+        });
     }
 
     /**
@@ -204,7 +214,10 @@ class DTOWriter {
         this.content = this.content.replace(PATTERN_TABLE_NAME, this.tableInfo.name);
         this.content = this.content.replace(PATTERN_ENGINE, this.tableInfo.engine);
         this.content = this.content.replace(PATTERN_PRIMARY_ID, this.primaryKey);
-        this.content = this.content.replace(PATTERN_FIELD_LIST, this.replaceFields.join(''));
+        this.replaceFields.forEach((fields, index) => {
+            const regExp = new RegExp(`FIELD_LIST_NO_${++index}`);
+            this.content = this.content.replace(regExp, fields.join(''));
+        });
     }
 
     /**
